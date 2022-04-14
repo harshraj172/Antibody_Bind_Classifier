964f9acaa27194cf295ecbd0e381580bd5461891
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-14 12:45:54 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,18 @@
-   
 import sys
 
 import numpy as np
 import torch
 
+import esm
 from dgl.nn.pytorch import GraphConv, NNConv
 from torch import nn
 from torch.nn import functional as F
 from typing import Dict, Tuple, List
 
-from equivariant_attention.modules import GConvSE3, GNormSE3, get_basis_and_r, GSE3Res, GMaxPooling, GAvgPooling
-from equivariant_attention.fibers import Fiber
+from Bind_Classifier.equivariant_attention.modules import GConvSE3, GNormSE3, get_basis_and_r, GSE3Res, GMaxPooling, GAvgPooling
+from Bind_Classifier.equivariant_attention.fibers import Fiber
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class TFN(nn.Module):
     """SE(3) equivariant GCN"""
@@ -139,11 +140,11 @@ class SE3Transformer(nn.Module):
         return h
 
 
-class Dual_SE3Transformer(nn.Module):
+class StructNet(nn.Module):
     """SE(3) equivariant GCN with attention"""
-    def __init__(self, num_layers: int, atom_feature_size: int, 
+    def __init__(self, num_layers: int, node_feature_size: int, edge_dim: int, 
                  num_channels: int, num_nlayers: int=1, num_degrees: int=4, 
-                 edge_dim: int=4, div: float=4, pooling: str='avg', n_heads: int=1, **kwargs):
+                 div: float=4, pooling: str='avg', n_heads: int=1, **kwargs):
         super().__init__()
         # Build the network
         self.num_layers = num_layers
@@ -155,7 +156,7 @@ class Dual_SE3Transformer(nn.Module):
         self.pooling = pooling
         self.n_heads = n_heads
 
-        self.fibers = {'in': Fiber(1, atom_feature_size),
+        self.fibers = {'in': Fiber(1, node_feature_size),
                        'mid': Fiber(num_degrees, self.num_channels),
                        'out': Fiber(1, num_degrees*self.num_channels)}
 
@@ -163,10 +164,14 @@ class Dual_SE3Transformer(nn.Module):
         blocks_AG = self._build_gcn(self.fibers)
         
         self.Gblock_AB, self.FCblock_AB = blocks_AB
-        self.Gblock_AG, self.FCblock_AG = blocks_AG 
-       
-        self.combine_FCBlock = self.combine_block(2*self.fibers['out'].n_features, 1)
-
+        self.Gblock_AG, self.FCblock_AG = blocks_AG
+        
+        layers = []
+        layers.append(nn.Linear(self.fibers['out'].n_features, 1))
+        layers.append(nn.ReLU(inplace=True))
+        self.combine_block = nn.ModuleList(layers)
+        
+        
     def _build_gcn(self, fibers):
         # Equivariant layers
         Gblock = []
@@ -188,17 +193,9 @@ class Dual_SE3Transformer(nn.Module):
         FCblock = []
         FCblock.append(nn.Linear(self.fibers['out'].n_features, self.fibers['out'].n_features))
         FCblock.append(nn.ReLU(inplace=True))
-#         FCblock.append(nn.Linear(self.fibers['out'].n_features, out_dim))
 
         return nn.ModuleList(Gblock), nn.ModuleList(FCblock)
 
-    def combine_block(self, in_dim, out_dim):
-        combine_FCblock = []
-        combine_FCblock.append(nn.Linear(in_dim, int(in_dim/2)))
-        combine_FCblock.append(nn.ReLU(inplace=True))
-        combine_FCblock.append(nn.Linear(int(in_dim/2), out_dim))
-        
-        return nn.ModuleList(combine_FCblock)
 
     def forward(self, G_AB, G_AG):
         # Compute equivariant weight basis from relative positions
@@ -213,11 +210,80 @@ class Dual_SE3Transformer(nn.Module):
 
         for i in range(len(self.FCblock_AB)):
             h_AB, h_AG = self.FCblock_AB[i](h_AB), self.FCblock_AG[i](h_AG)
-
-        # concatenate the hidden states
-        h = torch.cat((h_AB, h_AG), 1)
-
-        for layer in self.combine_FCBlock:
-            h = layer(h)
             
+        h = torch.diag(torch.matmul(h_AB, h_AG.permute(1, 0))).unsqueeze(-1)
+        
+        return h
+
+
+class get_SeqEmb:
+    def __init__(self, pretrained_model_name):
+        self.model, self.alphabet, self.batch_converter = self.load_pretrained(pretrained_model_name)
+    def load_pretrained(self, pretrained_model_name):
+        if pretrained_model_name=="esm1b_t33_650M_UR50S":
+            model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+            batch_converter = alphabet.get_batch_converter()
+            model = model.to(device)
+        return model, alphabet, batch_converter
+    
+    def pretrained_emb(self, data):
+        batch_labels, batch_strs, batch_tokens = self.batch_converter(data)
+        batch_tokens = batch_tokens.to(device)
+        # Extract per-residue representations (on CPU)
+        results = self.model(batch_tokens, repr_layers=[33], return_contacts=True)
+        token_representations = results["representations"][33]
+
+        # Generate per-sequence representations via averaging
+        # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
+        sequence_representations = []
+        for i, (_, seq) in enumerate(data):
+            sequence_representations.append(token_representations[i, 1 : len(seq) + 1].mean(0).unsqueeze(0))
+        return torch.cat(sequence_representations)
+    
+    
+class StructSeqNet(nn.Module):
+    def __init__(self, use_struct: bool, use_seq: bool, num_layers: int, node_feature_size: int, edge_dim: int,
+                 pretrained_lm_model: str='esm1b_t33_650M_UR50S', pretrained_lm_emb_dim: int=1280, num_channels: int=16, num_nlayers: int=1, 
+                 num_degrees: int=4, div: int=4, pooling: str='avg', n_heads: int=1, **kwargs):
+        super().__init__()
+        self.use_struct, self.use_seq = use_struct, use_seq
+        
+        self.StructModel = StructNet(num_layers, 
+                                   node_feature_size, 
+                                   edge_dim,
+                                   num_channels=num_channels,
+                                   num_nlayers=num_nlayers,
+                                   num_degrees=num_degrees,
+                                   div=div,
+                                   pooling=pooling,
+                                   n_heads=n_heads).to(device)
+        self.comblayers = nn.ModuleList([nn.Linear(2, 1),
+                                     nn.ReLU(inplace=True)])
+        
+        self.FCBlock_AB, self.FCBlock_AG = self.FCBlock(pretrained_lm_emb_dim), self.FCBlock(pretrained_lm_emb_dim)
+        
+    def FCBlock(self, in_dim):
+        layers = nn.ModuleList([nn.Linear(in_dim, int(in_dim/2)),
+                                nn.ReLU(inplace=True),
+                                nn.Linear(int(in_dim/2), int(in_dim/4)),
+                                nn.ReLU(inplace=True)])
+        return layers 
+    
+    def forward(self, gAB, hAB, gAG, hAG):
+        h1, h2 = torch.zeros(hAB.size(0), 1).to(device), torch.zeros(hAG.size(0), 1).to(device)
+                                
+        if self.use_struct:
+            h1 = self.StructModel(gAB, gAG)
+        if self.use_seq:
+            for i in range(len(self.FCBlock_AB)):
+                hAB, hAG = self.FCBlock_AB[i](hAB), self.FCBlock_AG[i](hAG)
+
+            # Dot product of hidden states
+            h2 = torch.diag(torch.matmul(hAB, hAG.permute(1, 0))).unsqueeze(-1)
+        
+        h = torch.cat((h1, h2), dim=1)
+                                
+        for layer in self.comblayers:
+            h = layer(h)
+                                
         return h
