d570997a406495a1331e1cbd02afe43bb5fb6757
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-15 01:13:20 +0530

    Update glob_utils.py

--- glob_utils.py
+++ glob_utils.py
@@ -9,8 +9,7 @@ def task_loss(pred, target, criterion, use_mean=True):
     if use_mean:
         l2_loss /= pred.shape[0]
 
-    rescale_loss = l1_loss # train_dataset.norm2units(l1_loss, FLAGS.task)
-    return l1_loss, l2_loss, rescale_loss
+    return l1_loss, l2_loss
     
 def metric(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torch.Tensor:
     '''Calculate F1 score. Can work with gpu tensors
@@ -21,13 +20,6 @@ def metric(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torch
     -------
     torch.Tensor
         `ndim` == 1. 0 <= val <= 1
-    
-    Reference
-    ---------
-    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
-    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
-    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
-    
     '''
     assert y_true.ndim == 1
     assert y_pred.ndim == 1 
