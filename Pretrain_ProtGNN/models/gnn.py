277804a0a7097d9ae4e4301f8aa8273b4d6d0758
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-19 00:47:58 +0530

    Update gnn.py

--- Pretrain_ProtGNN/models/gnn.py
+++ Pretrain_ProtGNN/models/gnn.py
@@ -1,7 +1,13 @@
+import re
+import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
 from torch_geometric.nn import GINConv, global_add_pool, GINEConv
+# from tape import ProteinBertModel, TAPETokenizer
+from transformers import BertModel
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def make_gine_conv(node_dim, edge_dim, out_dim):
     return GINEConv(nn=nn.Sequential(nn.Linear(node_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)), edge_dim=edge_dim)
@@ -39,9 +45,9 @@ class GConv(nn.Module):
         return z, g
 
 
-class Encoder(torch.nn.Module):
+class AugEncoder(torch.nn.Module):
     def __init__(self, encoder, augmentor):
-        super(Encoder, self).__init__()
+        super(AugEncoder, self).__init__()
         self.encoder = encoder
         self.augmentor = augmentor
 
@@ -53,3 +59,39 @@ class Encoder(torch.nn.Module):
         z1, g1 = self.encoder(x1, edge_index1, edge_weight1, batch)
         z2, g2 = self.encoder(x2, edge_index2, edge_weight2, batch)
         return z, g, z1, z2, g1, g2
+
+class DualEncoder(torch.nn.Module):
+    def __init__(self, encoderAB, encoderAG):
+        super(DualEncoder, self).__init__()
+        self.encoderAB = encoderAB
+        self.encoderAG = encoderAG
+        
+    def forward(self, xAB, edge_indexAB, edge_attrAB, batchAB,\
+                xAG, edge_indexAG, edge_attrAG, batchAG):
+        zAB, gAB = self.encoderAB(xAB, edge_indexAB, edge_attrAB, batchAB)
+        zAG, gAG = self.encoderAG(xAG, edge_indexAG, edge_attrAG, batchAG)
+        
+        return zAB, zAG, gAB, gAG
+    
+def tokenize(seqs, tokenizer, MAX_LEN=1024):
+    seqs = [" ".join(seq) for seq in seqs]
+    seqs = [re.sub(r"[UZOB]", "X", seq) for seq in seqs]
+    token_ids = torch.cat([tokenizer(seq, max_length=MAX_LEN,\
+                            truncation=True, padding='max_length',\
+                            return_tensors='pt')['input_ids'] for seq in seqs])
+    return token_ids
+
+class SeqEncoder(torch.nn.Module):
+    def __init__(self, pretrained_lm_model="Rostlab/prot_bert",\
+                 pretrained_lm_dim=526, project_dim=64):
+        super(SeqEncoder, self).__init__()
+        self.model = BertModel.from_pretrained(pretrained_lm_model)
+        self.project = torch.nn.Sequential(
+            nn.Linear(pretrained_lm_dim, project_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(project_dim, project_dim))
+    
+    def forward(self, token_ids):
+        output = self.model(token_ids)['pooler_output']
+        h = self.project(output)
+        return h 
