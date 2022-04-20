fa7a95d6ad5a54c033754bfb90e32ac35a936df9
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-20 10:54:35 +0530

    Update utils.py

--- Pretrain_ProtGNN/utils.py
+++ Pretrain_ProtGNN/utils.py
@@ -100,9 +100,9 @@ def prepare_data(path):
         
 def prepare_dataABAG(pathAB, pathAG):
     XAB = read_json(pathAB)
-    XAB = XAB[:100]
+    XAB = XAB
     XAG = read_json(pathAG)
-    XAG = XAG[:100]
+    XAG = XAG
     features = ProteinFeatures()
     data_lstAB, data_lstAG, sample_idx = [], [], 0
     print('Preparing Data...')
