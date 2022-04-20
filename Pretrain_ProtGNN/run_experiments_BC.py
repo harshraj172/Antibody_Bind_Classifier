1d5b73512df762a43049474de9947c073f0ec595
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-20 11:01:50 +0530

    Update run_experiments_BC.py

--- Pretrain_ProtGNN/run_experiments_BC.py
+++ Pretrain_ProtGNN/run_experiments_BC.py
@@ -57,6 +57,33 @@ def train(model, tokenizer, dataloaderAB, dataloaderAG, optimizer, device_ids):
     return epoch_loss/len(dataloaderAB), result_df
 
 
+def val(model, tokenizer, dataloaderAB, dataloaderAG, device_ids):
+    model.eval()
+    
+    epoch_loss = 0
+    Y_true, Y_pred = torch.tensor([]), torch.tensor([])
+    for (dataAB, dataAG) in zip(cycle(dataloaderAB), dataloaderAG):
+        (GAB, seqAB, y) = dataAB
+        (GAG, seqAG, y) = dataAG
+        tokenAB, tokenAG = tokenize(seqAB, tokenizer), tokenize(seqAG, tokenizer)
+        y = y.reshape(-1, 1).to(torch.float32)
+
+        pred = data_parallel(module=model, input=(GAB.cuda(), GAG.cuda(), tokenAB.cuda(), tokenAG.cuda()),\
+                             device_ids=device_ids)  
+        pred = pred.to("cpu")
+        # pred = model(GAB, GAG, tokenAB, tokenAG)
+        
+        loss, __ = BinaryClass_Loss(pred, y)
+
+        # for evaluation
+        Y_true = torch.cat((Y_true.to('cpu'), y.to('cpu')))
+        Y_pred = torch.cat((Y_pred.to('cpu'), pred.to('cpu')))
+        
+        epoch_loss += loss.item()
+            
+    result_df = metric(Y_pred.view(-1), Y_true.long().view(-1))
+    return epoch_loss/len(dataloaderAB), result_df
+
 def test(model, tokenizer, dataloaderAB, dataloaderAG, device_ids):
     model.eval()
     
@@ -84,16 +111,16 @@ def test(model, tokenizer, dataloaderAB, dataloaderAG, device_ids):
     result_df = metric(Y_pred.view(-1), Y_true.long().view(-1))
     return epoch_loss/len(dataloaderAB), result_df
 
-
 def main(
     # data params
     train_data_dir="data/SabDab/train",
     test_data_dir="data/SabDab/test",
+    val_data_dir="data/SabDab/val"
     device_ids="[0]",
     # trainer params
     batch_size=32,
     test_batch_size=10,
-    learning_rate=1e-3,
+    learning_rate=1e-7,
     epochs=50,
     early_stopping="valid_loss",
     # optimizer params
@@ -134,19 +161,25 @@ def main(
     
     # preparing the data 
     train_data_lstAB, train_data_lstAG = prepare_dataABAG(f'{train_data_dir}/XAb.json', f'{train_data_dir}/XAg.json')
-    train_loaderAB = DataLoader(train_data_lstAB[:200], batch_size=batch_size, shuffle=True)
+    train_loaderAB = DataLoader(train_data_lstAB, batch_size=batch_size, shuffle=True)
     del train_data_lstAB
     
     node_dim = max(train_data_lstAG[0][0].x.size(-1), 1)
     edge_dim = max(train_data_lstAG[0][0].edge_attr.size(-1), 1)
     
-    train_loaderAG = DataLoader(train_data_lstAG[:200], batch_size=batch_size, shuffle=True)
+    train_loaderAG = DataLoader(train_data_lstAG, batch_size=batch_size, shuffle=True)
     del train_data_lstAG
-  
+
+    val_data_lstAB, val_data_lstAG = prepare_dataABAG(f'{val_data_dir}/XAb.json', f'{val_data_dir}/XAg.json')
+    val_loaderAB = DataLoader(val_data_lstAB, batch_size=test_batch_size, shuffle=True)
+    del val_data_lstAB
+    val_loaderAG = DataLoader(val_data_lstAG, batch_size=val_batch_size, shuffle=True)
+    del val_data_lstAG
+    
     test_data_lstAB, test_data_lstAG = prepare_dataABAG(f'{test_data_dir}/XAb.json', f'{test_data_dir}/XAg.json')
-    test_loaderAB = DataLoader(test_data_lstAB[-200:], batch_size=test_batch_size, shuffle=True)
+    test_loaderAB = DataLoader(test_data_lstAB, batch_size=test_batch_size, shuffle=True)
     del test_data_lstAB
-    test_loaderAG = DataLoader(test_data_lstAG[-200:], batch_size=test_batch_size, shuffle=True)
+    test_loaderAG = DataLoader(test_data_lstAG, batch_size=test_batch_size, shuffle=True)
     del test_data_lstAG
     
     # structure model
@@ -163,10 +196,11 @@ def main(
                                 use_struct=use_struct, use_seq=use_seq).cuda() 
     
     optimizer = optim.AdamW(structseq_enc.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
     
-    # training
     with tqdm(total=epochs-1, desc='(T)') as pbar:
         for epoch in range(epochs):
+            # training
             train_loss, train_result_df = train(structseq_enc, tokenizer, train_loaderAB,\
                                                 train_loaderAG, optimizer, device_ids)
             if log_wandb:
@@ -178,13 +212,23 @@ def main(
             pbar.set_postfix({'loss': train_loss})
             pbar.update()
     
-    # testing
-    test_loss, test_result_df = test(structseq_enc, tokenizer, test_loaderAB, test_loaderAG, device_ids)
-    print(f"Test Loss = {test_loss}")
-    if log_wandb:
-        wandb.log({"test BCE loss": test_loss,
-                   "test precision": test_result_df['Precision'][0],
-                   "test recall": test_result_df['Recall'][0], 
-                   "test F1 score": test_result_df['F1 Score'][0]})
+            # validation
+            val_loss, val_result_df = val(structseq_enc, tokenizer, val_loaderAB, val_loaderAG, device_ids)
+            print(f"val Loss = {val_loss}")
+            if log_wandb:
+                wandb.log({"val BCE loss": val_loss,
+                           "val precision": val_result_df['Precision'][0],
+                           "val recall": val_result_df['Recall'][0], 
+                           "val F1 score": val_result_df['F1 Score'][0]})
+            scheduler.step(val_loss)
+                
+        # testing
+        val_loss, val_result_df = val(structseq_enc, tokenizer, val_loaderAB, val_loaderAG, device_ids)
+        print(f"val Loss = {val_loss}")
+        if log_wandb:
+            wandb.log({"val BCE loss": val_loss,
+                       "val precision": val_result_df['Precision'][0],
+                       "val recall": val_result_df['Recall'][0], 
+                       "val F1 score": val_result_df['F1 Score'][0]})
     
     return test_result_df['F1 Score'][0]
