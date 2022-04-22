bcbea396792f1409b376c8e6bdd9e3dd1a08dd31
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-22 19:31:37 +0530

    Update and rename requirements.txt to init.sh

---
+++ init.sh
@@ -0,0 +1,16 @@
+pip install biopython
+pip install prody
+pip install fair-esm
+pip install packaging
+pip install git+https://github.com/AMLab-Amsterdam/lie_learn
+# pip install networkx
+pip install PyGCL
+pip install wandb
+pip install torch-geometric
+pip install torch-scatter
+pip install torch-sparse
+pip install dgl-cu110
+# wandb login
+pip install tape_proteins
+pip install transformers
+pip install sentencepiece
