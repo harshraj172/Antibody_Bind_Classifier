d5a6a2ed7aa5650e243eb5884cbee987b03a209a
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-22 13:42:34 +0530

    Update train_se3.py

--- train_se3.py
+++ train_se3.py
@@ -24,7 +24,7 @@ from glob_utils import *
 def to_np(x):
     return x.cpu().detach().numpy()
 
-def train_epoch(epoch, model, criterion, dataloader, optimizer, scheduler, FLAGS):
+def train_epoch(epoch, model, criterion, dataloader, optimizer, scheduler, device_ids, FLAGS):
     model.train()
   
     num_iters = len(dataloader)
@@ -34,32 +34,31 @@ def train_epoch(epoch, model, criterion, dataloader, optimizer, scheduler, FLAGS
         tokenAB, tokenAG = tokenize(seqAB), tokenize(seqAG)
         tokenAB = tokenAB.cuda()
         tokenAG = tokenAG.cuda()
-        gAB = gAB.to(FLAGS.device)
-        gAG = gAG.to(FLAGS.device)
+        gAB = gAB.to("cuda")
+        gAG = gAG.to("cuda")
         y = y.cuda()
 
         optimizer.zero_grad()
-
         # run model forward and compute loss
-        pred = model(gAB, tokenAB, gAG, tokenAG)
-    
+        # pred = model(gAB, tokenAB, gAG, tokenAG)
+        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+                             device_ids=device_ids)
+
         loss = criterion(pred, y)
         rloss += loss
         
         # backprop
         loss.backward()
         optimizer.step()
-
+        # scheduler.step(epoch + i / num_iters)
+        
         if i % FLAGS.print_interval == 0:
             print(f"[{epoch}|{i}] train Loss/step: {loss:.5f}")
         if FLAGS.use_wandb:
-            if i % FLAGS.log_interval == 0:
-                wandb.log({"train loss/step": to_np(loss)})
+            wandb.log({"train loss/step": to_np(loss)})
 
         if FLAGS.profile and i == 10:
             sys.exit()
-
-        scheduler.step(epoch + i / num_iters)
 
         # for evaluation
         Y_true = torch.cat((Y_true.to('cpu'), y.to('cpu')))
@@ -84,12 +83,14 @@ def val_epoch(epoch, loss_fnc, criterion, dataloader, FLAGS):
         tokenAB, tokenAG = tokenize(seqAB), tokenize(seqAG)
         tokenAB = tokenAB.cuda()
         tokenAG = tokenAG.cuda()
-        gAB = gAB.to(FLAGS.device)
-        gAG = gAG.to(FLAGS.device)
+        gAB = gAB.to("cuda")
+        gAG = gAG.to("cuda")
         y = y.cuda()
 
         # run model forward and compute loss
-        pred = model(gAB, tokenAB, gAG, tokenAG).detach()
+        # pred = model(gAB, tokenAB, gAG, tokenAG)
+        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+                             device_ids=device_ids)
         loss = criterion(pred, y)
         rloss += loss
         
@@ -106,6 +107,7 @@ def val_epoch(epoch, loss_fnc, criterion, dataloader, FLAGS):
                    "val precision": results_df['Precision'][0],
                    "val recall": results_df['Recall'][0], 
                    "val F1 score": results_df['F1 Score'][0]})
+    return results_df['F1 Score'][0]
 
 def test_epoch(epoch, model, loss_fnc, criterion, dataloader, FLAGS):
     model.eval()
@@ -116,12 +118,14 @@ def test_epoch(epoch, model, loss_fnc, criterion, dataloader, FLAGS):
         tokenAB, tokenAG = tokenize(seqAB), tokenize(seqAG)
         tokenAB = tokenAB.cuda()
         tokenAG = tokenAG.cuda()
-        gAB = gAB.to(FLAGS.device)
-        gAG = gAG.to(FLAGS.device)
+        gAB = gAB.to("cuda")
+        gAG = gAG.to("cuda")
         y = y.cuda()
 
         # run model forward and compute loss
-        pred = model(gAB, hAB, gAG, hAG).detach()
+        # pred = model(gAB, tokenAB, gAG, tokenAG)
+        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+                             device_ids=device_ids)
         loss = criterion(pred, y)
         rloss += loss
         
@@ -149,8 +153,40 @@ def collate(samples):
     return batched_graphAB, seqAB_lst, batched_graphAG, seqAG_lst, torch.tensor(y)
 
 
-def main(FLAGS, UNPARSED_ARGV):
+def main(FLAGS,
+         learning_rate,
+         num_layers,
+         num_degrees,
+         num_channels,
+         num_nlayers,
+         div,
+         pooling,
+         head,
+         device_ids,):
 
+    # wandb logging
+    config = {
+      "num_layers": num_layers,
+      "num_degrees": num_degrees,
+      "num_channels": num_channels,
+      "num_nonliner_layers": num_nlayers,
+      "head": head,
+      "batch size": FLAGS.batch_size,
+      "learning_rate": learning_rate,
+      "epochs": FLAGS.num_epochs,
+    }
+    
+    # Fix name
+    if not FLAGS.name:
+        FLAGS.name = f'E-d{num_degrees}-l{num_layers}-{num_channels}-{num_nlayers}'
+    
+    if FLAGS.use_wandb:
+        # Log all args to wandb
+        if FLAGS.name:
+            wandb.init(config=config, project=f'{FLAGS.wandb}', name=f'{FLAGS.name}', entity="harsh1729")
+        else:
+            wandb.init(config=config, project=f'{FLAGS.wandb}', entity="harsh1729")    
+    
     # Prepare data
     train_dataset = Antibody_Antigen_Dataset(f'{FLAGS.train_data_dir}/XAb.json', f'{FLAGS.train_data_dir}/XAg.json')
     train_loader = DataLoader(train_dataset, 
@@ -180,30 +216,30 @@ def main(FLAGS, UNPARSED_ARGV):
     # Choose model
     model = models.__dict__.get(FLAGS.model)(FLAGS.use_struct, 
                                              FLAGS.use_seq,
-                                             FLAGS.num_layers, 
+                                             num_layers, 
                                              train_dataset.node_feature_size, 
                                              train_dataset.edge_feature_size,
-                                             num_channels=FLAGS.num_channels,
-                                             num_nlayers=FLAGS.num_nlayers,
-                                             num_degrees=FLAGS.num_degrees,
-                                             div=FLAGS.div,
-                                             pooling=FLAGS.pooling,
-                                             n_heads=FLAGS.head,
+                                             num_channels=num_channels,
+                                             num_nlayers=num_nlayers,
+                                             num_degrees=num_degrees,
+                                             div=div,
+                                             pooling=pooling,
+                                             n_heads=head,
                                              pretrained_lm_model=FLAGS.pretrained_lm_model, 
                                              pretrained_lm_dim=FLAGS.pretrained_lm_dim)
 
 
     if FLAGS.restore is not None:
         model.load_state_dict(torch.load(FLAGS.restore))
-    model.to(FLAGS.device)
+    model = model.cuda()
     #wandb.watch(model)
 
     # Optimizer settings
-    optimizer = optim.AdamW(model.parameters(), lr=FLAGS.lr)
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
     
     # Loss
-    criterion = torch.nn.BCEWithLogitsLoss(reduction='sum')
+    criterion = torch.nn.BCELoss(reduction='sum')
 
     # Save path
     save_path = os.path.join(FLAGS.save_dir, FLAGS.name + '.pt')
@@ -214,124 +250,8 @@ def main(FLAGS, UNPARSED_ARGV):
         torch.save(model.state_dict(), save_path)
         print(f"Saved: {save_path}")
 
-        train_epoch(epoch, model, criterion, train_loader, optimizer, scheduler, FLAGS)
-        val_epoch(epoch, model, criterion, val_loader, FLAGS)
-        test_epoch(epoch, model, criterion, test_loader, FLAGS)
-
-        
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()\
+        train_epoch(epoch, model, criterion, train_loader, optimizer, scheduler, device_ids, FLAGS)
+        f1_score = val_epoch(epoch, model, criterion, val_loader, device_ids, FLAGS)
+        test_epoch(epoch, model, criterion, test_loader, device_ids, FLAGS)
     
-    parser.add_argument('--use_seq', type=eval, default=False,
-            help="Use sequence info of protein")
-    parser.add_argument('--use_struct', type=eval, default=True,
-            help="Use structure info of protein")
-    
-    # Model parameters
-    parser.add_argument('--model', type=str, default='StructSeqEnc', 
-            help="String name of model")
-    parser.add_argument('--pretrained_lm_model', type=str, default="Rostlab/prot_bert",
-            help="Pretrained LM model name")
-    parser.add_argument('--pretrained_lm_dim', type=int, default=1024,
-            help="Pretrained LM model out dim")
-    
-    parser.add_argument('--num_layers', type=int, default=1,
-            help="Number of equivariant layers") #4
-    parser.add_argument('--num_degrees', type=int, default=2,
-            help="Number of irreps {0,1,...,num_degrees-1}") #4
-    parser.add_argument('--num_channels', type=int, default=4,
-            help="Number of channels in middle layers") #16
-    parser.add_argument('--num_nlayers', type=int, default=1,
-            help="Number of layers for nonlinearity")
-    parser.add_argument('--fully_connected', action='store_true',
-            help="Include global node in graph")
-    parser.add_argument('--div', type=float, default=4,
-            help="Low dimensional embedding fraction")
-    parser.add_argument('--pooling', type=str, default='avg',
-            help="Choose from avg or max")
-    parser.add_argument('--head', type=int, default=1,
-            help="Number of attention heads")
-
-    # Meta-parameters
-    parser.add_argument('--batch_size', type=int, default=1, 
-            help="Batch size")
-    parser.add_argument('--lr', type=float, default=1e-3, 
-            help="Learning rate")
-    parser.add_argument('--num_epochs', type=int, default=10, 
-            help="Number of epochs")
-
-    # Data
-    parser.add_argument('--train_data_dir', type=str, default='data/SabDab/train',
-            help="training data directory Antibodies")
-    parser.add_argument('--val_data_dir', type=str, default='data/SabDab/test',
-            help="validation data directory Antibodies")
-    parser.add_argument('--test_data_dir', type=str, default='data/SabDab/test',
-            help="validation data directory Antibodies")
-
-
-    # Logging
-    parser.add_argument('--name', type=str, default=None,
-            help="Run name")
-    parser.add_argument('--use_wandb', type=eval, default=True,
-            help="To use wandb or not - [True, False]")
-    parser.add_argument('--log_interval', type=int, default=25,
-            help="Number of steps between logging key stats")
-    parser.add_argument('--print_interval', type=int, default=250,
-            help="Number of steps between printing key stats")
-    parser.add_argument('--save_dir', type=str, default="models",
-            help="Directory name to save models")
-    parser.add_argument('--restore', type=str, default=None,
-            help="Path to model to restore")
-    parser.add_argument('--wandb', type=str, default='Bind-Classifier', 
-            help="wandb project name")
-
-    # Miscellanea
-    parser.add_argument('--num_workers', type=int, default=0, 
-            help="Number of data loader workers")
-    parser.add_argument('--profile', action='store_true',
-            help="Exit after 10 steps for profiling")
-
-    # Random seed for both Numpy and Pytorch
-    parser.add_argument('--seed', type=int, default=None)
-
-    FLAGS, UNPARSED_ARGV = parser.parse_known_args()
-
-    # Fix name
-    if not FLAGS.name:
-        FLAGS.name = f'E-d{FLAGS.num_degrees}-l{FLAGS.num_layers}-{FLAGS.num_channels}-{FLAGS.num_nlayers}'
-
-    # Create model directory
-    if not os.path.isdir(FLAGS.save_dir):
-        os.makedirs(FLAGS.save_dir)
-
-    # Fix seed for random numbers
-    if not FLAGS.seed: FLAGS.seed = 1992 #np.random.randint(100000)
-    torch.manual_seed(FLAGS.seed)
-    np.random.seed(FLAGS.seed)
-
-    # Automatically choose GPU if available
-    FLAGS.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-
-    print("\n\nFLAGS:", FLAGS)
-    print("UNPARSED_ARGV:", UNPARSED_ARGV, "\n\n")
-    
-    config = {
-      "num_layers": FLAGS.num_layers,
-      "num_degrees": FLAGS.num_degrees,
-      "num_channels": FLAGS.num_channels,
-      "num_nonliner_layers": FLAGS.num_nlayers,
-      "head": FLAGS.head,
-      "batch size": FLAGS.batch_size,
-      "learning_rate": FLAGS.lr,
-      "epochs": FLAGS.num_epochs,
-    }
-
-    if FLAGS.use_wandb:
-        # Log all args to wandb
-        if FLAGS.name:
-            wandb.init(config=config, project=f'{FLAGS.wandb}', name=f'{FLAGS.name}', entity="harsh1729")
-        else:
-            wandb.init(config=config, project=f'{FLAGS.wandb}', entity="harsh1729")
-
-    # Where the magic is
-    main(FLAGS, UNPARSED_ARGV)
+    return f1_score
