3f7ccf024b85cd6b375fa8d5e025cd58ff3412e4
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-22 15:03:24 +0530

    Update train_se3.py

--- train_se3.py
+++ train_se3.py
@@ -40,9 +40,9 @@ def train_epoch(epoch, model, criterion, dataloader, optimizer, scheduler, devic
 
         optimizer.zero_grad()
         # run model forward and compute loss
-        # pred = model(gAB, tokenAB, gAG, tokenAG)
-        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
-                             device_ids=device_ids)
+        pred = model(gAB, tokenAB, gAG, tokenAG)
+        # pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+        #                      device_ids=device_ids)
 
         loss = criterion(pred, y)
         rloss += loss
@@ -74,7 +74,7 @@ def train_epoch(epoch, model, criterion, dataloader, optimizer, scheduler, devic
                    "train recall": results_df['Recall'][0], 
                    "train F1 Score": results_df['F1 Score'][0]})
 
-def val_epoch(epoch, loss_fnc, criterion, dataloader, FLAGS):
+def val_epoch(epoch, model, criterion, dataloader, device_ids, FLAGS):
     model.eval()
 
     rloss = 0
@@ -88,9 +88,9 @@ def val_epoch(epoch, loss_fnc, criterion, dataloader, FLAGS):
         y = y.cuda()
 
         # run model forward and compute loss
-        # pred = model(gAB, tokenAB, gAG, tokenAG)
-        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
-                             device_ids=device_ids)
+        pred = model(gAB, tokenAB, gAG, tokenAG)
+        # pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+        #                      device_ids=device_ids)
         loss = criterion(pred, y)
         rloss += loss
         
@@ -109,7 +109,7 @@ def val_epoch(epoch, loss_fnc, criterion, dataloader, FLAGS):
                    "val F1 score": results_df['F1 Score'][0]})
     return results_df['F1 Score'][0]
 
-def test_epoch(epoch, model, loss_fnc, criterion, dataloader, FLAGS):
+def test_epoch(epoch, model, criterion, dataloader, device_ids, FLAGS):
     model.eval()
 
     rloss = 0
@@ -123,9 +123,9 @@ def test_epoch(epoch, model, loss_fnc, criterion, dataloader, FLAGS):
         y = y.cuda()
 
         # run model forward and compute loss
-        # pred = model(gAB, tokenAB, gAG, tokenAG)
-        pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
-                             device_ids=device_ids)
+        pred = model(gAB, tokenAB, gAG, tokenAG)
+        # pred = data_parallel(module=model, input=(gAB, tokenAB, gAG, tokenAG),\
+        #                      device_ids=device_ids)
         loss = criterion(pred, y)
         rloss += loss
         
@@ -255,3 +255,111 @@ def main(FLAGS,
         test_epoch(epoch, model, criterion, test_loader, device_ids, FLAGS)
     
     return f1_score
+
+
+def run_exp():
+    parser = argparse.ArgumentParser()\
+    
+    parser.add_argument('--use_seq', type=eval, default=False,
+            help="Use sequence info of protein")
+    parser.add_argument('--use_struct', type=eval, default=True,
+            help="Use structure info of protein")
+    
+    # Model parameters
+    parser.add_argument('--model', type=str, default='StructSeqEnc', 
+            help="String name of model")
+    parser.add_argument('--pretrained_lm_model', type=str, default="Rostlab/prot_bert",
+            help="Pretrained LM model name")
+    parser.add_argument('--pretrained_lm_dim', type=int, default=1024,
+            help="Pretrained LM model out dim")
+    
+    parser.add_argument('--nlayers_range', type=str, default="[2, 6]",
+            help="Number of equivariant layers") #4
+    parser.add_argument('--ndegrees_range', type=str, default="[2, 6]",
+            help="Number of irreps {0,1,...,num_degrees-1}") #4
+    parser.add_argument('--nchannels_range', type=str, default="[8, 16]",
+            help="Number of channels in middle layers") #16
+    parser.add_argument('--nnlayers_range', type=str, default="[1, 4]",
+            help="Number of layers for nonlinearity")
+    parser.add_argument('--fully_connected', action='store_true',
+            help="Include global node in graph")
+    parser.add_argument('--div_range', type=str, default="[2, 4]",
+            help="Low dimensional embedding fraction")
+    parser.add_argument('--pool_lst', type=str, default="[avg, max]",
+            help="Choose from avg or max")
+    parser.add_argument('--head_range', type=str, default="[1, 4]",
+            help="Number of attention heads")
+
+    # Meta-parameters
+    parser.add_argument('--batch_size', type=int, default=1, 
+            help="Batch size")
+    parser.add_argument('--lr_range', type=str, default="[1e-5, 1e-3]", 
+            help="Learning rate")
+    parser.add_argument('--num_epochs', type=int, default=150, 
+            help="Number of epochs")
+
+    # Data
+    parser.add_argument('--train_data_dir', type=str, default='data/SabDab/train',
+            help="training data directory Antibodies")
+    parser.add_argument('--val_data_dir', type=str, default='data/SabDab/test',
+            help="validation data directory Antibodies")
+    parser.add_argument('--test_data_dir', type=str, default='data/SabDab/test',
+            help="validation data directory Antibodies")
+
+
+    # Logging
+    parser.add_argument('--name', type=str, default=None,
+            help="Run name")
+    parser.add_argument('--use_wandb', type=eval, default=True,
+            help="To use wandb or not - [True, False]")
+    parser.add_argument('--log_interval', type=int, default=25,
+            help="Number of steps between logging key stats")
+    parser.add_argument('--print_interval', type=int, default=250,
+            help="Number of steps between printing key stats")
+    parser.add_argument('--save_dir', type=str, default="models",
+            help="Directory name to save models")
+    parser.add_argument('--restore', type=str, default=None,
+            help="Path to model to restore")
+    parser.add_argument('--wandb', type=str, default='Bind-Classifier', 
+            help="wandb project name")
+
+    # Miscellaneas
+    parser.add_argument('--device_ids', type=str, default="[0]", 
+            help="The cuda device id")
+    parser.add_argument('--num_workers', type=int, default=0, 
+            help="Number of data loader workers")
+    parser.add_argument('--profile', action='store_true',
+            help="Exit after 10 steps for profiling")
+
+    # Random seed for both Numpy and Pytorch
+    parser.add_argument('--seed', type=int, default=None)
+
+    FLAGS, UNPARSED_ARGV = parser.parse_known_args()
+
+    # Create model directory
+    if not os.path.isdir(FLAGS.save_dir):
+        os.makedirs(FLAGS.save_dir)
+
+    # Fix seed for random numbers
+    if not FLAGS.seed: FLAGS.seed = 1992 #np.random.randint(100000)
+    torch.manual_seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+
+    # device_ids(str -> list(int))
+    device_ids = [int(k) for k in FLAGS.device_ids.strip('][').split(', ')]
+    
+    f1_score = main(
+                 FLAGS,
+                 learning_rate=1e-3,
+                 num_layers=2,
+                 num_degrees=4,
+                 num_channels=4,#16
+                 num_nlayers=1,
+                 div=4,
+                 pooling="avg",
+                 head=1,
+                 device_ids=device_ids,
+                 )
+    return f1_score
+
+run_exp()
